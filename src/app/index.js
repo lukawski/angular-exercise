3dd4e875c9927024678b469a8796f718aed1ed1a
Kacper Łukawski <ka.lukawski@gmail.com>
2017-12-20 21:49:42 +0100

    General styles

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,13 @@
 import angular from 'angular';
 import ngResource from 'angular-resource'
 
-//import '../style/index.css';
+import '../style/index.css';
 import controllersModule from './modules/controllers';
 import servicesModule from './modules/services';
 
 console.log(servicesModule);
 
-angular.module('app', [
+angular.module('tablesModule', [
   ngResource,
   controllersModule.name,
   servicesModule.name
