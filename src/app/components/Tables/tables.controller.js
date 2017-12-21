8f429aaccb8787e2fa8feb4637e245162a250260
Kacper Łukawski <ka.lukawski@gmail.com>
2017-12-21 01:00:18 +0100

    Added search inputs for both tables, implemented logic

--- src/app/components/Tables/tables.controller.js
+++ src/app/components/Tables/tables.controller.js
@@ -1,9 +1,23 @@
-function TablesController(Books, Writers, TablesApiService) {
+function TablesController(Books, Writers, TablesApiService, $filter) {
+  let books = [];
+  let writers = [];
+
   TablesApiService.getWritersAndBooks()
     .then(({ transformedBooks, transformedWriters }) => {
       this.books = transformedBooks;
       this.writers = transformedWriters;
+
+      books = transformedBooks;
+      writers = transformedWriters;
     });
+
+  this.filterBooks = function(searchText) {
+    this.books = $filter('filter')(books, searchText);
+  }
+
+  this.filterWriters = function(searchText) {
+    this.writers = $filter('filter')(writers, searchText);
+  }
 }
 
 export default TablesController;
