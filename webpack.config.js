5d291202cb055039ded14ee8d0a61612b6a32eed
Kacper Łukawski <ka.lukawski@gmail.com>
2017-12-22 13:25:37 +0100

    Added production configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
+const ENV = process.env.APP_ENV;
+const isProd = ENV === 'prod';
+
+const config = {
   devtool: 'source-map',
   entry: __dirname + '/src/app/index.js',
   output: {
@@ -36,11 +40,22 @@ module.exports = {
       template: __dirname + '/src/public/index.html',
       inject: 'body'
     }),
-    new ExtractTextPlugin('styles.css'),
-    new DashboardPlugin()
+    new ExtractTextPlugin('styles.css')
   ],
   devServer: {
     contentBase: './src/public',
     port: 7700
   }
 };
+
+if (isProd) {
+  config.plugins.push(
+    new UglifyJSPlugin()
+  );
+} else {
+  config.plugins.push(
+    new DashboardPlugin()
+  )
+}
+
+module.exports = config;
